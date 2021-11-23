07e90d4cbdb95285ca956146719c0aef361462c0
Ilia-Kosenkov <Ilia-Kosenkov@users.noreply.github.com>
2021-11-23 09:38:30 +0000

    deploy: 98604cc316524433957422fbdf0aa3d38055516b

---
+++ extendr_api/scalar/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Rfloat","Rfloat is a wrapper for f64 in the context of an R’s integer vector."],["Rint","Rint is a wrapper for i32 in the context of an R’s integer vector."]]});
